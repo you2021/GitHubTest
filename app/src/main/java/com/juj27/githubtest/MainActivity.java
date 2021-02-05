5ad04e5bc810a243a24caf75ff79cd9017973880
you2021 <78577077+you2021@users.noreply.github.com>
2021-02-05 14:20:21 +0900

    Update MainActivity.java

--- app/src/main/java/com/juj27/githubtest/MainActivity.java
+++ app/src/main/java/com/juj27/githubtest/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Github");
+        
+        int a;
     }
 }
